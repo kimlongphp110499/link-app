5cf6bfb463b318c32aea347d4fc552bca2e4d6f5
doquangtrung <43035930+doquangtrung@users.noreply.github.com>
2025-04-18 20:38:51 +0700

    [TDQ] remove login add point clan

--- app/Http/Controllers/Api/UserVoteLinkController.php
+++ app/Http/Controllers/Api/UserVoteLinkController.php
@@ -8,9 +8,7 @@ use App\Models\ClanTempMember;
 use Illuminate\Http\Request;
 use App\Models\Link;
 use App\Models\VoteHistory;
-use App\Models\ClanPointHistory;
 use App\Models\User;
-use App\Models\Clan;
 use App\Services\LinkService;
 use App\Services\ClanPointHistoryService;
 use Illuminate\Support\Facades\Log;
@@ -34,67 +32,53 @@ class UserVoteLinkController extends Controller
         $this->clanPointHistoryService = $clanPointHistoryService;
     }
 
-    // Phương thức vote cho link
-    public function vote(Request $request, $linkId)
+    public function vote(Request $request, $linkId): \Illuminate\Http\JsonResponse
     {
         $userId = auth()->user()->id;
         $user = User::findOrFail($userId);
-
         $request->validate([
             'points' => 'required|integer|min:1',
         ]);
-
-        // Kiểm tra nếu user tồn tại
         if (!$user) {
             return response()->json(['message' => 'User not found'], 404);
         }
 
-        // Kiểm tra nếu link tồn tại
         $link = Link::where('video_id', $linkId)->first();
-
         if (!$link) {
             return response()->json(['message' => 'Link not found'], 404);
         }
 
-        // Kiểm tra số điểm người dùng đủ để vote (ví dụ mỗi lần vote trừ đi 100 điểm)
         $pointsRequired = $request->points;
         if ($user->points < $pointsRequired) {
             return response()->json(['message' => 'Not enough points to vote'], 400);
         }
 
-        // Trừ điểm của người dùng
         $user->points -= $pointsRequired;
         $user->save();
-
         $link->total_votes = $link->total_votes + $request->points;
         $link->save();
-        // Lưu lịch sử vote vào bảng vote_histories
         $voteHistory = VoteHistory::create([
             'user_id' => $user->id,
             'link_id' => $link->id,
             'points_voted' => $pointsRequired,
         ]);
-
         if ($voteHistory) {
             $clans =  ClanLink::select('clan_id')->where('link_id', $link->id)->get();
             foreach ($clans as $clan) {
-                $exitUserClan = $this->clanPointHistoryService->existingHistory($userId, $clan->clan_id);
-                if (!$exitUserClan) {
-                    try {
-                        ClanTempMember::create([
-                            'user_id' => $user->id,
-                            'link_id' => $link->id,
-                            'clan_id' => $clan->clan_id,
-                        ]);
-                    } catch (\Exception $e) {
-                        Log::error('Unexpected error while fetching honors: ' . $e->getMessage(), [
-                            'exception' => $e->getTraceAsString(),
-                        ]);
-                        return response()->json([
-                            'status' => 'error',
-                            'message' => $e->getMessage(),
-                        ], 500);
-                    }
+                try {
+                    ClanTempMember::create([
+                        'user_id' => $user->id,
+                        'link_id' => $link->id,
+                        'clan_id' => $clan->clan_id,
+                    ]);
+                } catch (\Exception $e) {
+                    Log::error('Unexpected error while fetching honors: ' . $e->getMessage(), [
+                        'exception' => $e->getTraceAsString(),
+                    ]);
+                    return response()->json([
+                        'status' => 'error',
+                        'message' => $e->getMessage(),
+                    ], 500);
                 }
             }
         }
@@ -108,50 +92,39 @@ class UserVoteLinkController extends Controller
         ], 200);
     }
 
-    // Lịch sử vote của người dùng
-    public function voteHistory()
+    public function voteHistory(): \Illuminate\Http\JsonResponse
     {
         $auth = auth()->user();
-        // Kiểm tra nếu user tồn tại, nếu không trả về lỗi 404
         $user = User::find($auth->id);
         if (!$user) {
             return response()->json(['message' => 'User not found'], 404);
         }
 
-        // Truy vấn vote history của người dùng và chỉ lấy những thông tin cần thiết
         $voteHistories = $user->voteHistories()
             ->with(['link' => function ($query) {
-                $query->select('id', 'title', 'url'); // Lấy chỉ các trường cần thiết từ bảng links
+                $query->select('id', 'title', 'url');
             }])
-            ->select('id', 'user_id', 'link_id', 'points_voted', 'created_at') // Chỉ lấy các trường cần thiết từ bảng vote_histories
+            ->select('id', 'user_id', 'link_id', 'points_voted', 'created_at')
             ->get();
 
-        // Trả về danh sách vote histories của người dùng
         return response()->json($voteHistories, 200);
     }
 
-    public function rankLinks()
+    public function rankLinks(): \Illuminate\Http\JsonResponse
     {
-        $links = $this->linkService->videoRank();
 
-        return response()->json($links, 200);
+        return response()->json($this->linkService->videoRank(), 200);
     }
 
-    public function searchLinks(Request $request)
+    public function searchLinks(Request $request): \Illuminate\Http\JsonResponse
     {
-        $query = $request->get('query'); // Lấy query từ tham số tìm kiếm
-
-        // Kiểm tra nếu query tồn tại
+        $query = $request->get('query');
         if ($query) {
-            // Loại bỏ khoảng trắng trong query
             $normalizedQuery = str_replace(' ', '', $query);
-
-            // Tìm kiếm các link theo tiêu đề hoặc ID video
             $links = Link::whereRaw("REPLACE(title, ' ', '') LIKE ?", ["%{$normalizedQuery}%"])
                 ->orWhereRaw("REPLACE(video_id, ' ', '') LIKE ?", ["%{$normalizedQuery}%"])
                 ->get();
         } else {
-            // Nếu không có query, trả về tất cả các link
             $links = Link::all();
         }
 
--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Link;
+use App\Models\ClanPointHistory;
+use App\Models\ClanTempMember;
+use Illuminate\Support\Facades\DB;
 use App\Models\Schedule;
 use App\Models\User;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class VideoController extends Controller
 {
@@ -13,42 +16,66 @@ class VideoController extends Controller
     public function getCurrentVideo()
     {
         $now = Carbon::now();
-        \Log::info("Current time: " . $now->toIso8601String());
-
-        // Lấy schedule hiện tại
+        Log::info("Current time: " . $now->toIso8601String());
         $currentSchedule = Schedule::first();
-        
         if (!$currentSchedule) {
             return response()->json(['message' => 'No video scheduled'], 404);
         }
-        
 
         $link = $currentSchedule->link;
         $userWithMaxVotes = $link->voteHistories()
             ->selectRaw('user_id, SUM(points_voted) as total_points')
             ->groupBy('user_id')
-            ->orderByDesc('total_points') // Lấy điểm cao nhất
+            ->orderByDesc('total_points')
             ->first();
 
-        // Gán thông tin user và điểm vote
-        $link->user_with_max_votes = $userWithMaxVotes 
-            ? User::find($userWithMaxVotes->user_id) 
+        $link->user_with_max_votes = $userWithMaxVotes
+            ? User::find($userWithMaxVotes->user_id)
             : null;
-        $link->user_max_vote_points = $userWithMaxVotes 
-            ? (int) $userWithMaxVotes->total_points 
+        $link->user_max_vote_points = $userWithMaxVotes
+            ? (int) $userWithMaxVotes->total_points
             : 0;
-
         $startTime = Carbon::parse($currentSchedule->start_time);
         $elapsedMilliseconds = (int)$now->diffInMilliseconds($startTime);
-
-        // Giả sử duration là một trường trong model Link, chuyển sang mili giây
         $durationMilliseconds = $link->duration ? $link->duration * 1000 : 0;
-        return response()->json([
-            'link' =>$link,
-            'offset' => $elapsedMilliseconds,
-            'start_time' => $startTime,
-            'duration' => $durationMilliseconds,
-            'timestamp' => $now->toIso8601String(),
-        ]);
+        try {
+            DB::beginTransaction();
+            $memberClan = ClanTempMember::select('user_id', 'clan_id')
+                    ->where('link_id', $link->id)
+                    ->get();
+            if ($memberClan->isEmpty()) {
+                return response()->json([
+                    'message' => 'No members found for the given link_id',
+                ], 404);
+            }
+
+            $dataToInsert = [];
+            foreach ($memberClan as $member) {
+                $dataToInsert[] = [
+                    'user_id' => $member->user_id,
+                    'clan_id' => $member->clan_id,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            }
+            ClanPointHistory::insert($dataToInsert);
+            ClanTempMember::where('link_id', $link->id)
+                     ->delete();
+            DB::commit();
+
+            return response()->json([
+                'link' =>$link,
+                'offset' => $elapsedMilliseconds,
+                'start_time' => $startTime,
+                'duration' => $durationMilliseconds,
+                'timestamp' => $now->toIso8601String(),
+            ]);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'message' => 'Error processing',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
     }
 }
