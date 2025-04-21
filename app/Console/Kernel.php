da6f32487d3ae070432f7fd404aea580be7839a2
Long Kim <kimlongphp110499>
2025-04-21 08:43:05 +0700

    fix schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('schedules:update')->everyFiveSeconds();
+        $schedule->command('schedules:update')->everyTwoSeconds();
         $schedule->command('log:clear')->everyFourHours();
     }
 
--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Link;
+use App\Models\ClanPointHistory;
+use App\Models\ClanTempMember;
+use Illuminate\Support\Facades\DB;
 use App\Models\Schedule;
 use App\Models\User;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class VideoController extends Controller
 {
@@ -13,84 +16,70 @@ class VideoController extends Controller
     public function getCurrentVideo()
     {
         $now = Carbon::now();
-        \Log::info("Current time: " . $now->toIso8601String());
-
-        // Lấy schedule hiện tại
-        $currentSchedule = Schedule::where('start_time', '<=', $now)
-            ->orderBy('start_time', 'desc')
-            ->first();
-        
+        Log::info("Current time: " . $now->toIso8601String());
+        $currentSchedule = Schedule::first();
         if (!$currentSchedule) {
             return response()->json(['message' => 'No video scheduled'], 404);
         }
-
-        $link = $currentSchedule->link;
-        $userWithMaxVotes = $link->voteHistories()
-            ->selectRaw('user_id, SUM(points_voted) as total_points')
-            ->groupBy('user_id')
-            ->orderByDesc('total_points') // Lấy điểm cao nhất
-            ->first();
-
-        // Gán thông tin user và điểm vote
-        $link->user_with_max_votes = $userWithMaxVotes 
-            ? User::find($userWithMaxVotes->user_id) 
-            : null;
-        $link->user_max_vote_points = $userWithMaxVotes 
-            ? (int) $userWithMaxVotes->total_points 
-            : 0;
-
-        $startTime = Carbon::parse($currentSchedule->start_time);
-        $elapsedMilliseconds = (int)$now->diffInMilliseconds($startTime);
-
-        // Giả sử duration là một trường trong model Link, chuyển sang mili giây
-        $durationMilliseconds = $link->duration ? $link->duration * 1000 : 0;
-        return response()->json([
-            'link' =>$link,
-            'offset' => $elapsedMilliseconds,
-            'duration' => $durationMilliseconds,
-            'timestamp' => $now->toIso8601String(),
-        ]);
-    }
-
-    public function getCurrentVideoTest()
-    {
         $now = Carbon::now();
-        \Log::info("Current time: " . $now->toIso8601String());
-
-        // Lấy schedule hiện tại
-        $currentSchedule = Schedule::where('start_time', '<=', $now)
-            ->orderBy('start_time', 'desc')
-            ->first();
-        
-        if (!$currentSchedule) {
-            return response()->json(['message' => 'No video scheduled'], 404);
-        }
 
         $link = $currentSchedule->link;
         $userWithMaxVotes = $link->voteHistories()
             ->selectRaw('user_id, SUM(points_voted) as total_points')
             ->groupBy('user_id')
-            ->orderByDesc('total_points') // Lấy điểm cao nhất
+            ->orderByDesc('total_points')
             ->first();
 
-        // Gán thông tin user và điểm vote
-        $link->user_with_max_votes = $userWithMaxVotes 
-            ? User::find($userWithMaxVotes->user_id) 
+        $link->user_with_max_votes = $userWithMaxVotes
+            ? User::find($userWithMaxVotes->user_id)
             : null;
-        $link->user_max_vote_points = $userWithMaxVotes 
-            ? (int) $userWithMaxVotes->total_points 
+        $link->user_max_vote_points = $userWithMaxVotes
+            ? (int) $userWithMaxVotes->total_points
             : 0;
-
         $startTime = Carbon::parse($currentSchedule->start_time);
         $elapsedMilliseconds = (int)$now->diffInMilliseconds($startTime);
-
-        // Giả sử duration là một trường trong model Link, chuyển sang mili giây
+        
         $durationMilliseconds = $link->duration ? $link->duration * 1000 : 0;
-        return response()->json([
-            'link' =>$link,
-            'offset' => $elapsedMilliseconds,
-            'duration' => $durationMilliseconds,
-            'timestamp' => $now->toIso8601String(),
-        ]);
+        $wait = $elapsedMilliseconds + $durationMilliseconds;
+         // Kiểm tra nếu start_time lớn hơn thời gian hiện tại
+         if ($wait < 0) {
+             return response()->json(['message' => 'Please wait'], 202); // Trả về thông báo "Hãy chờ"
+         }
+
+        try {
+            DB::beginTransaction();
+            $memberClan = ClanTempMember::select('user_id', 'clan_id')
+                    ->where('link_id', $link->id)
+                    ->get();
+            if (!$memberClan->isEmpty()) {
+                $dataToInsert = [];
+                foreach ($memberClan as $member) {
+                    $dataToInsert[] = [
+                        'user_id' => $member->user_id,
+                        'clan_id' => $member->clan_id,
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ];
+                }
+                ClanPointHistory::insert($dataToInsert);
+                ClanTempMember::where('link_id', $link->id)
+                         ->delete();
+            }
+            DB::commit();
+
+            return response()->json([
+                'link' =>$link,
+                'offset' => $elapsedMilliseconds,
+                'start_time' => $startTime,
+                'duration' => $durationMilliseconds,
+                'timestamp' => $now->toIso8601String(),
+            ]);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'message' => 'Error processing',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
     }
 }
